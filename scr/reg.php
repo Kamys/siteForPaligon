fa5d5e990e438c014aebf261ef5abddfd724dc0a
Hikita Kamyshenko <hnkntoc@gmail.com>
2017-04-12 10:24:47 +0300

    Develop.

--- scr/reg.php
+++ scr/reg.php
@@ -39,21 +39,5 @@ function logging($messages, $args)
     echo "<br>";
 }
 ?>
-
-<form action="reg.php" method="post">
-    <h2> Ателье ПО "Содружество"</h2>
-    <h3>Вход на сайт</h3>
-    <div>
-        <input placeholder="Имя" name="user_name" type="text">
-    </div>
-    <div>
-        <input placeholder="Пароль" name="user_password" type="password">
-    </div>
-    <div>
-        <input value="Войти" type="submit">
-        <a href="#">Регистрация</a>
-    </div>
-</form>
-
 </body>
 </html>
